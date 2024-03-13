4f3038976502d29484e855ca35a8cd35087ccf35
Oleg Bakharev <disconnectix@gmail.com>
2024-03-13 19:25:29 +0300

    added Option structure with their methods

--- source/optibrev.d
+++ source/optibrev.d
@@ -1,6 +1,72 @@
-import std.stdio;
+module optibrev;
 
-void main()
+// Option type
+struct Option(T) 
 {
-	writeln("Edit source/app.d to start your project.");
+	private 
+	{	
+		// type of stored value
+		enum OptionType : byte 
+	    {
+	        NONE,
+	        SOME
+	    }
+	    	
+		OptionType type;
+		T value;
+		
+		this(OptionType type, T value = T.init)
+		{
+			this.type  = type;
+			this.value = value;
+		}
+	}
+	
+	// is None ?
+	bool isNone() {
+		return type == OptionType.NONE;
+	}
+	
+	// is Some ?
+	bool isSome() {
+		return type == OptionType.SOME;
+	}
+	
+	// return Option if Some or return defaultValue otherwise
+	T orElse(T defaultValue)
+	{
+		return (type == OptionType.NONE) ? defaultValue : value;
+	}
+	
+	// None type
+	Option!T None()
+	{
+		return Option!T(OptionType.NONE);
+	}
+	
+	// Some value
+	Option!T Some(T value)
+	{
+		return Option!T(OptionType.SOME, value);
+	}
+	
+	// string representation
+    string toString() const 
+	{
+		import std.conv : to;
+		import std.format : format;
+		
+		return (type == OptionType.NONE) ? "None" : format("Some(%s)", to!string(value));
+	}
+    
+    // unwrap value
+    T unwrap() const 
+    {
+        if (type == OptionType.SOME)
+        {
+            return value;
+        }
+        
+        throw new Exception("Can't unwrap None"); 
+    }
 }
